15bcd9db0cedb77080313ecb2832b5cc99546516
saadsgh123 <sdsghouri@gmail.com>
2024-02-29 21:30:42 +0100

    task 9 try 2

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+	if (tree)
+	{
+		size_t left = 0, right = 0;
 
-if (tree == NULL)
-{
-return (0);
-}
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-return (MAX(left_height, right_height) + 1);
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left > right) ? left : right);
+	}
+	return (0);
 }
