ce4655de8950f71a4f94de37f8db8fc4ece5deb5
saadsgh123 <sdsghouri@gmail.com>
2024-02-29 22:20:09 +0100

    task 10 try 1

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,12 +8,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + binary_tree_depth(tree->parent));
-	}
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
