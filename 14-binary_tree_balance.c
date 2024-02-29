4fe6422843ba4f9ec6a14534c71d1be9358a381b
saadsgh123 <sdsghouri@gmail.com>
2024-02-29 21:40:01 +0100

    task 14 try 1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,8 +26,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t left_node = 0, right_node = 0;
 
-        left_node = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-        right_node = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	left_node = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	right_node = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((left_node > right_node) ? left_node : right_node);
 	}
 	return (0);
